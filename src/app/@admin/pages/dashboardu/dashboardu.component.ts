2ce1cc4da5f5b09ae9eb6a356d81f4110264f6c9
git-progamador-novato <86372383+git-progamador-novato@users.noreply.github.com>
2021-07-01 18:19:02 -0400

    primier comit

---
+++ src/app/@admin/pages/dashboardu/dashboardu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardu',
+  templateUrl: './dashboardu.component.html',
+  styleUrls: ['./dashboardu.component.scss']
+})
+export class DashboarduComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
